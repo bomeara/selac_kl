6f8ab77c0f8a58ea87e26700a401fe2530a4a618
Brian O'Meara <omeara.brian@gmail.com>
2017-08-02 09:48:48 -0400

    test run

--- scripts/StartRuns.R
+++ scripts/StartRuns.R
@@ -1,13 +1,16 @@
 library(selac)
 library(ape)
+source("BasicSimulationScripts.R")
 
 load("../data/yeast.practiceSELACunrestgamma106.Rdata")
 
 start.rep = 1
-stop.rep = 2
+end.rep = 2
 true.model = result
-SimulateData(true.model=true.model, start.rep=start.rep, stop.rep=stop.rep)
+rm(result)
+SimulateData(true.model=true.model, start.rep=start.rep, end.rep=end.rep)
 
-for (sim.index in start.rep:stop.rep) {
-  AnalyzeDataWithKnownValues(true.model, paste("fastaSet",sim.index, sep="_")
+for (sim.index in start.rep:end.rep) {
+  result <- AnalyzeDataWithKnownValues(true.model, paste("fastaSet",sim.index, sep="_")
+  save(result, file=paste0("SimResult_",sim.index,".rda"))
 }
