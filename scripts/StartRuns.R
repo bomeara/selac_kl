da01862b3a288513bf1bdc2e2b979af803dcabd3
Brian OMeara <omeara.brian@gmail.com>
2017-08-09 09:56:23 -0400

    runs

--- scripts/StartRuns.R
+++ scripts/StartRuns.R
@@ -11,6 +11,8 @@ rm(result)
 SimulateData(true.model=true.model, start.rep=start.rep, end.rep=end.rep)
 
 for (sim.index in start.rep:end.rep) {
+  print(Sys.time())
+  print(paste0("Starting run ", sim.index))
   result <- AnalyzeDataEstimatingValues(true.model, paste0(paste("fastaSet",sim.index, sep="_"),'/'))
   save(result, file=paste0("SimResult_",sim.index,".rda"))
 }
