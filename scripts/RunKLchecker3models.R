4c9e71758c5d60d1cfb15dd24765a1d6985dd8e7
Brian OMeara <omeara.brian@gmail.com>
2017-08-30 17:37:50 -0400

    still debugging gtr

--- scripts/RunKLchecker3models.R
+++ scripts/RunKLchecker3models.R
@@ -4,7 +4,7 @@ library(ape)
 library(geiger)
 library(TreeSim)
 rm(list=ls())
-options(error = utils::recover)
+#options(error = utils::recover)
 
 
 #
@@ -69,6 +69,39 @@ likelihood.data.with.candidate.model <- function(data) {
 }
 
 
+RunIndividual <- function(model.K.vector, k.index, param.estimator.candidate.model.JC, param.estimator.candidate.model.HKY, TreeSim, sim.bd.taxa, ntax.vector, j, phangorn, simSeq, nchar.vector, i, seed.fit, simulator.true.model, simulator.candidate.model, likelihood.data.with.true.model, likelihoods.data.with.candidate.model, results, rep) {
+    K <- model.K.vector[k.index]
+    param.estimator.candidate.model <- param.estimator.candidate.model.JC
+    if(k.index==2) {
+        param.estimator.candidate.model <- param.estimator.candidate.model.HKY
+    }
+    new.data <- NULL
+    tree <- NULL
+    nrow.new.data <- 0
+    attempts <- 1
+    #    while(nrow.new.data<=3 & attempts<50) { #need variation
+    tree <- TreeSim::sim.bd.taxa(n=ntax.vector[j], numbsim=1, lambda=1, mu=0.9, frac=0.5, complete=FALSE)[[1]]
+    tree$edge.length <-  tree$edge.length/(max(branching.times(tree))) #so have less evolution; if too little, starts adding more
+    new.data <- phangorn::simSeq(x=tree, l=nchar.vector[i], Q=seed.fit$Q, bf=seed.fit$bf, type="DNA" , rate=0.01 * attempts)
+    nrew.new.data <- nrow(as.character(unique(new.data)))
+    attempts <- attempts+1
+    #print(attempts)
+    #      if(attempts>=50) {
+    #        new.data <- NULL
+    #      }
+    #    }
+    #    if(!is.null(new.data)) {
+    fit = pml(tree, new.data)
+    
+    fit = optim.pml(fit, optNni=FALSE, model="GTR", optGamma=FALSE, optQ=TRUE)
+    true.parameters <- fit
+    model.comparison <- EstimateKL(simulator.true.model, true.parameters, param.estimator.candidate.model, simulator.candidate.model, likelihood.data.with.true.model, likelihoods.data.with.candidate.model, K, nrep.outer=50, nrep.inner=50)
+    results <- rbind(results, data.frame(nchar=nchar.vector[i], ntax=ntax.vector[j], KL=model.comparison[1], AIC=model.comparison[2], rep=rep, K=K))
+    save(results, file="KLrun3model.rda")
+    print(results)
+    return(results)
+}
+
 nchar.vector <- c(3,4,5,10,20,50,100,200)
 ntax.vector <- c(4,5,6,7,8,9,10, 25, 50, 100)
 results <- data.frame()
@@ -82,43 +115,21 @@ for (rep in sequence(10)) {
   for (i in sequence(length(nchar.vector))) {
     for (j in sequence(length(ntax.vector))) {
       for (k.index in sequence(model.K.vector)) {
+
         #
         #new.data <- Laurasiatherian
       #  new.data <- as.phyDat(as.character(new.data)[,1:nchar.vector[i]])
         #tree = nj(dist.ml(new.data))
       #  tree <- geiger::drop.random(tree, Ntip(tree) - ntax.vector[j])
-        try({
-          K <- model.K.vector[k.index]
-          param.estimator.candidate.model <- param.estimator.candidate.model.JC
-          if(k.index==2) {
-            param.estimator.candidate.model <- param.estimator.candidate.model.HKY
+          results.this <- NULL
+        try(results.this <- RunIndividual(model.K.vector, k.index, param.estimator.candidate.model.JC, param.estimator.candidate.model.HKY, TreeSim, sim.bd.taxa, ntax.vector, j, phangorn, simSeq, nchar.vector, i, seed.fit, simulator.true.model, simulator.candidate.model, likelihood.data.with.true.model, likelihoods.data.with.candidate.model, results, rep))
+          if(!is.null(results.this)) {
+            results <- results.this   
           }
-          new.data <- NULL
-          tree <- NULL
-          nrow.new.data <- 0
-          attempts <- 1
-          while(nrow.new.data!=ntax.vector[j] & attempts<50) { #need variation
-            tree <- TreeSim::sim.bd.taxa(n=ntax.vector[j], numbsim=1, lambda=1, mu=0.9, frac=0.5, complete=FALSE)[[1]]
-            tree$edge.lengths <-  tree$edge.lengths/(max(branching.times(tree))) #so have less evolution; if too little, starts adding more
-            new.data <- phangorn::simSeq(x=tree, l=nchar.vector[i], Q=seed.fit$Q, bf=seed.fit$bf, type="DNA" , rate=0.01 * attempts)
-            nrew.new.data <- nrow(as.character(unique(new.data)))
-            attempts <- attempts+1
-            #print(attempts)
-            if(attempts>=50) {
-              new.data <- NULL
-            }
-          }
-          if(!is.null(new.data)) {
-            fit = pml(tree, new.data)
 
-            fit = optim.pml(fit, optNni=FALSE, model="GTR", optGamma=FALSE, optQ=TRUE)
-            true.parameters <- fit
-            model.comparison <- EstimateKL(simulator.true.model, true.parameters, param.estimator.candidate.model, simulator.candidate.model, likelihood.data.with.true.model, likelihoods.data.with.candidate.model, K, nrep.outer=50, nrep.inner=50)
-            results <- rbind(results, data.frame(nchar=nchar.vector[i], ntax=ntax.vector[j], KL=model.comparison[1], AIC=model.comparison[2], rep=rep, K=K))
-            save(results, file="KLrun3model.rda")
-            print(results)
-          }
-        })
+      #    } else {
+      #      print(paste0("Tried but failed nchar = ", nchar.vector[i], " ntax = ", ntax.vector[j], " model K = ", K))
+    #      }
       }
     }
   }
